2033c657dc443e0110128a30c87f63c7e679d152
Tatae1199 <68005947+Tatae1199@users.noreply.github.com>
2020-07-08 18:14:03 +0700

    Add files via upload

---
+++ งานชิ้น1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{  cout<<"     #   "<<endl;
+   cout<<"    # #"<<endl;
+   cout<<"   #   #"<<endl;
+   cout<<"  #     #"<<endl;
+   cout<<"  #######"<<endl;
+   cout<<"  #     #"<<endl;
+   cout<<"Jitkawee Ruamrattanasin"<<endl;
+   return(0);
+}
